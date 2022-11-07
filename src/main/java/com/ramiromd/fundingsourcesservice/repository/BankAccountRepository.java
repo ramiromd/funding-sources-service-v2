d22446ccd8389597797661e8c233fc931dce236d
Ramiro Martínez D'Elía <ramiro.m@geopagos.com>
2022-11-07 19:42:18 -0300

    Add annotation

--- src/main/java/com/ramiromd/fundingsourcesservice/repository/BankAccountRepository.java
+++ src/main/java/com/ramiromd/fundingsourcesservice/repository/BankAccountRepository.java
@@ -2,6 +2,8 @@ package com.ramiromd.fundingsourcesservice.repository;
 
 import com.ramiromd.fundingsourcesservice.entity.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
 }
--- src/main/java/com/ramiromd/fundingsourcesservice/repository/CreditCardRepository.java
+++ src/main/java/com/ramiromd/fundingsourcesservice/repository/CreditCardRepository.java
@@ -2,7 +2,8 @@ package com.ramiromd.fundingsourcesservice.repository;
 
 import com.ramiromd.fundingsourcesservice.entity.CreditCard;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CreditCardRepository extends JpaRepository<CreditCard, Long> {
 }
