6ee7cb3a2dbd6ce8292895a26c490320cc9a2bd4
Ramiro Martínez D'Elía <ramiro.m@geopagos.com>
2022-11-13 18:03:37 -0300

    Extract payloads to file

--- src/test/java/com/ramiromd/fundingsourcesservice/unit/data/request/CreateSourceDeserializationTest.java
+++ src/test/java/com/ramiromd/fundingsourcesservice/unit/data/request/CreateSourceDeserializationTest.java
@@ -7,20 +7,19 @@ import com.ramiromd.fundingsourcesservice.data.request.CreateCreditCardDto;
 import com.ramiromd.fundingsourcesservice.helper.ObjectMapperFactory;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CreateSourceDeserializationTest {
 
     @Test
-    public void it_can_deserialize_a_credit_card_request() throws JsonProcessingException {
+    public void it_can_deserialize_a_credit_card_request() throws IOException {
         ObjectMapper mapper = ObjectMapperFactory.getDefaultMapper();
-        String payload = "{\"type\": \"credit_card\", " +
-                "\"name\": \"My first credit card\", " +
-                "\"number\": \"3428-3071-3218-6112\", " +
-                "\"expirationDate\": \"11/26\", " +
-                "\"cardholderName\": \"Ramiro Martinez D'Elía\"}";
+        File input = new File("src/test/resources/stub/create_credit_card_request_1.json");
 
-        CreateCreditCardDto dto = mapper.readValue(payload, CreateCreditCardDto.class);
+        CreateCreditCardDto dto = mapper.readValue(input, CreateCreditCardDto.class);
         assertEquals(dto.getType(), SourceType.CREDIT_CARD);
         assertEquals(dto.getName(), "My first credit card");
         assertEquals(dto.getNumber(), "3428-3071-3218-6112");
